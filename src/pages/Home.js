6b845301f8ffb9aeb8e7b3b69b8d8e67153ffa2e
André Posmitny <andre.lifter@gmail.com>
2024-10-04 18:12:33 +0100

    Added advanced styles to PurchaseButton using MUI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Button from '@mui/material/Button'; // Импорт кнопки из MUI
-
+import Button from '@mui/material/Button';
 function Home() {
   return (
     <div>
       <h2>Welcome to the Individual Spiritual Development Program</h2>
       <Button variant="contained" color="primary">
-        Get a Hug
+        BECOME A CHAMPION
       </Button>
     </div>
   );
