fe7e26d2069a7b92e54509329ccf80cba85e373b
André Posmitny <andre.lifter@gmail.com>
2024-10-06 17:11:35 +0100

    Updated header with a mint background for a fresh look

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 function Header() {
   return (
-    <header>
-      <h1>Spiritual Development Program</h1>
-    </header>
+    <AppBar position="static" style={{ backgroundColor: '#e0f7fa' }}> {/* Нежный светло-голубой фон */}
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{ color: '#333' }}>
+          Spiritual Development Program
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
