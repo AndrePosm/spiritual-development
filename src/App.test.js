82fb084e693bec793c2294041cd99f1dd0c46406
André Posmitny <andre.lifter@gmail.com>
2024-10-21 18:34:37 +0100

    Fix tests and update Babel and Jest configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Spiritual Development Program', () => {
+  render(<App />);  // Используем только render, act автоматически обрабатывается
+
+  const titleElements = screen.getAllByText(/Spiritual Development Program/i);
+  expect(titleElements.length).toBeGreaterThan(0); // Проверяем, что хотя бы один элемент найден
 });
