0a8a8dec86df7fe33af15fabe0d9f2b06ec0ca4e
André Posmitny <andre.lifter@gmail.com>
2024-11-24 18:13:37 +0000

    Add: Created and styled animated breathing bubble component

--- src/pages/BreathBubble.js
+++ src/pages/BreathBubble.js
@@ -1,9 +1,12 @@
+import React from "react";
+import "./BreathBubble.css";
+
 function BreathBubble() {
   return (
     <div className="breath-bubble">
       <h2>Breath Bubble</h2>
       <p>This is the Breath Bubble page.</p>
-      <div className="bubble"></div>
+      <div className="bubble"></div> {/* Новый элемент для "дышащего" круга */}
     </div>
   );
 }
